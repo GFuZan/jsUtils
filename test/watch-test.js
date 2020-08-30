6f9a10228525eb671b658e9ac3243dbcc49c33fa
gfuzan <gfuzan@163.com>
2020-08-30 10:11:19 +0800

    监听工具 追加配置项

--- test/watch-test.js
+++ test/watch-test.js
@@ -3,58 +3,88 @@ const { watch, unWatch } = require('../utils/watch')
 
 // 数据准备
 const data = {
-    a: 1,
-    b: 1,
-    c: 1,
-    get d(){
-      return this.c
-    },
-    set d(val) {
-      this.c = val;
-      this.b = val
-    }
+  a: 1,
+  b: 1,
+  c: 1,
+  get d() {
+    return this.c
+  },
+  set d(val) {
+    this.c = val;
+    this.b = val
   }
+}
 
-  watch(data, ['x'], (newVal, oldVal, key)=>{
-    console.log(`监听未定义值: X = ${data['x']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
-  })
+watch(data, ['x'], (newVal, oldVal, key) => {
+  console.log(`监听未定义值: X = ${data['x']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
+})
 
-  // 监听a的变化
-  watch(data, ['a'], (newVal, oldVal, key)=>{
-    console.log(`监听A: A = ${data['a']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
-  }, {
-    immediate: true,
-    handleKey: '哈哈1'
-  })
+// handleKey对照
+// 监听a的变化
+watch(data, ['a'], (newVal, oldVal, key) => {
+  console.log(`监听A: A = ${data['a']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
+}, {
+  immediate: true,
+  handleKey: '哈哈1'
+})
 
-  // 监听a的变化
-  watch(data, ['a'], (newVal, oldVal, key)=>{
-    console.log(`监听A: A = ${data['a']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
-  }, {
-    immediate: true,
-    handleKey: '哈哈'
-  })
+// 监听a的变化
+watch(data, ['a'], (newVal, oldVal, key) => {
+  console.log(`监听A: A = ${data['a']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
+}, {
+  immediate: true,
+  handleKey: '哈哈'
+})
 
-  // 监听a,b,c三个值得变化
-  watch(data, ['a','b','c'], (newVal, oldVal, key)=>{
-    console.log(`监听多值: ${data['a']}-${data['b']}-${data['c']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
-  })
+// 监听a,b,c三个值得变化
+watch(data, ['a', 'b', 'c'], (newVal, oldVal, key) => {
+  console.log(`监听多值: ${data['a']}-${data['b']}-${data['c']}, newVal = ${newVal}, oldVal = ${oldVal}, key = ${key}`);
+})
+
+// 同步/异步对照
+watch(data, ['y'], () => {
+  console.log(`异步监听: y = ${data['y']}. y2 = ${data['y2']}`);
+}, {
+  async: true,
+  asyncTime: 1
+})
+watch(data, ['y'], () => {
+  // 由于先加y, 后加y2, 同步监听取y2时, y2还未变化
+  console.log(`同步监听: y = ${data['y']}. y2 = ${data['y2']}`);
+}, {
+  async: false
+})
+
+// 数据变化回调对照
+watch(data, ['z'], (newVal, oldVal) => {
+  console.log(`赋值监听: z = ${data['z']}, newVal = ${newVal}, oldVal = ${oldVal}`);
+}, {
+  changeTrigger: false
+})
+watch(data, ['z'], (newVal, oldVal) => {
+  console.log(`变化监听: z = ${data['z']}, newVal = ${newVal}, oldVal = ${oldVal}`);
+}, {
+  changeTrigger: true
+})
 
 
-  // 监听d的变化
-  const wk =  watch(data, ['d'], ()=>{
-    console.log(`监听D: D = ${data['d']}`);
-  })
+// 监听d的变化
+const wk = watch(data, ['d'], () => {
+  console.log(`监听D: D = ${data['d']}`);
+})
 
 
-  // 取消监听
-  setTimeout(()=>{
-    unWatch(wk)
-  }, 6000)
+// 取消监听
+setTimeout(() => {
+  unWatch(wk)
+}, 6000)
 
 
-  setInterval(()=>{
-    data.a++
-    data.d++
-    data.x = (data.x || 0) + 1
-  }, 2000)
+setInterval(() => {
+  data.a++
+  data.d++
+  data.x = (data.x || 0) + 1
+  data.y = (data.y || 0) + 1
+  data.y2 = (data.y2 || 0) + 1
+  data.y % 2 ? (data.z = (data.z || 0) + 1) : (data.z = data.z)
+}, 2000)
--- utils/watch.js
+++ utils/watch.js
@@ -2,7 +2,7 @@
 var dataKey = '$__VUE__DATA__'
 var watchListKey = '$__VUE__WATCH__LIST__'
 
-function Vue () { }
+function Vue() { }
 
 /**
  * @param {*} watchObject 监听对象
@@ -18,16 +18,37 @@ function Vue () { }
  *
  * @typedef {Object} WatchConfig 监听配置
  * @property {Boolean?} immediate 是否添加监听后立刻执行, 执行时回调值不存在, 默认false
+ * @property {Boolean?} async 是否异步执行, 默认false
+ * @property {Number?} asyncTime 异步执行延时,异步执行时有效, 默认1
+ * @property {Boolean?} changeTrigger 只有数据发生变化时才触发回调, 默认false
  * @property {String|Number?} handleKey 指定处理方法的唯一标识, 用于监听去重,不指定时,默认为handle
  */
 Vue.prototype.watch = function (watchObject, watchKeys, handle, config) {
     try {
         // 配置
-        config = config || {
-            immediate: false,
-            handleKey: null
-        }
-    
+        config = config || {}
+
+        /**
+         * @type {WatchHanle}
+         */
+        var handleExcute = (function () {
+            // 作用域闭包
+            return function (newVal, oldVal, key) {
+                // 立即执行时,key为undefined,此处是排除立即执行
+                if (config.changeTrigger && key !== undefined && newVal === oldVal) {
+                    return
+                }
+                var _watchCxcute = function () {
+                    try {
+                        handle(newVal, oldVal, key)
+                    } catch (error) {
+                        console.warn('监听执行异常', error, watchList[handle])
+                    }
+                }
+                return !config.async ? _watchCxcute() : setTimeout(_watchCxcute, config.asyncTime || 1)
+            }
+        })()
+
         var vueWatch = null
         watchObject && (!watchObject[watchListKey] && (Object.defineProperty(watchObject, watchListKey, {
             configurable: false,
@@ -50,43 +71,35 @@ Vue.prototype.watch = function (watchObject, watchKeys, handle, config) {
                     watchObject[key] = undefined
                 }
                 keyProperty.enumerable = false,
-                keyProperty.configurable = false
-    
+                    keyProperty.configurable = false
+
                 Object.defineProperty(watchObject, vueDataKey, keyProperty)
                 // 创建监听
                 Object.defineProperty(watchObject, key, {
-                    get () {
+                    get() {
                         return watchObject[vueDataKey]
                     },
-                    set (newVal) {
+                    set(newVal) {
                         var oldVal = watchObject[vueDataKey]
                         watchObject[vueDataKey] = newVal
                         // 执行监听队列
-                        for (var handle in watchList) {
-                            if (watchList[handle]) {
-                                try {
-                                    watchList[handle](newVal, oldVal, key)
-                                } catch (error) {
-                                    console.warn('监听执行异常', error, watchList[handle])
-                                }
+                        for (var _handle in watchList) {
+                            if (watchList[_handle]) {
+                                watchList[_handle](newVal, oldVal, key)
                             }
                         }
                     }
                 })
             }
             // 添加处理至监听队列
-            vueWatch[key][config.handleKey || String(handle)] = handle
+            vueWatch[key][config.handleKey || String(handle)] = handleExcute
         })
-    
+
         // 立即执行
         if (config.immediate) {
-            try {
-                handle && handle()
-            } catch (error) {
-                console.warn('监听执行异常', error, handle)
-            }
+            handleExcute()
         }
-    
+
         return {
             watchObject: watchObject,
             watchKeys: watchKeys,
